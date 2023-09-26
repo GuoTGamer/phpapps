f6b667c2c976c1526b45ddfd983848c348cb1cee
MRUselessPotato <svendejong10.sdj@gmail.com>
2023-09-26 11:50:10 +0200

    V1 ifkeuze

---
+++ ifkeuze.php
@@ -0,0 +1,20 @@
+<?php
+
+$naam = readline("Geef jouw naam : ");
+$leeftijd = readline("Geef jouw leeftijd : ");
+
+echo "Hallo $naam \n";
+
+if ( $leeftijd < 22 ) {
+        echo "Jij ben jonger dan 22 jaar.\n";
+}
+if ( $leeftijd > 22 ) {
+        echo "Jij ben jonger dan 22 jaar.\n";
+}
+if ( $leeftijd == 22 ) {
+        echo "Jij ben jonger dan 22 jaar.\n";
+}
+
+
+
+?>
