a4d7e092baf67a5a8a0adf3d7fed6679f02b2f47
MRUselessPotato <svendejong10.sdj@gmail.com>
2023-09-26 09:17:41 +0200

    reken

---
+++ reken.php
@@ -0,0 +1,21 @@
+<?php
+
+$getal1=15;
+$getal2=3;
+
+$som=$getal1 + $getal2;
+echo "$getal1 + $getal2 = ".$som."\n";
+
+$verschil=$getal1 - $getal2;
+echo "$getal1 - $getal2 = ".$verschil."\n";
+
+$product=$getal1 * $getal2;
+echo "$getal1 * $getal2 = ".$product."\n";
+
+$quotient=$getal1 / $getal2;
+echo "$getal1 / $getal2 = ".$quotient."\n";
+
+$rest=$getal1 % $getal2;
+echo "$getal1 % $getal2 = ".$rest."\n";
+
+?>
